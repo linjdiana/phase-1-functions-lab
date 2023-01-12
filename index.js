bb27730a0976ccc673178dceffa1d3df393d89d4
Diana Lin <linjdiana@gmail.com>
2023-01-12 08:46:34 -0800

    almost lab time

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(value) {
+    if (value > 42) return value - 42;
+    else (value < 42)
+    return 42 - value;
+    //returns the number of blocks given a value
+  }
+
+  function distanceFromHqInBlocks(value) {
+    if (value > 42) return (value - 42) * 264;
+    else (value < 42)
+    return (value - 42) * 264;
+  }
